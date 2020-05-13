f9a418b27945c57f5959794dee01d9e537bf3b46
Maran Siemons <Maran@MacBook-Pro-van-Maran.local>
2020-05-13 11:19:15 +0200

    comments bonus

--- bonus/floor_ceiling_texture_bonus.c
+++ bonus/floor_ceiling_texture_bonus.c
@@ -6,26 +6,16 @@
 /*   By: Maran <Maran@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/30 14:12:22 by Maran         #+#    #+#                 */
-/*   Updated: 2020/05/12 23:41:53 by Maran         ########   odam.nl         */
+/*   Updated: 2020/05/13 11:17:35 by Maran         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d_bonus.h"
 
 /*
-** -CELL: the cell coord is got from the integer parts of floorx and floory
-** -TX/TY: get the texture coordinate from the fractional part
-** -OWN PART: reversed dest formula harm.
-** ceiling is symmetrical, at (screenHeight - y - 1) instead of y.
+** Cell: the cell coord is got from the integer parts of floorx and floory
+** Tx/ty: get the texture coordinate
 */
-
-	
-	// base->tex_f.png_img = mlx_png_file_to_image(base->mlx.mlx, "./64x/dirt.png", &base->game.texWidth, &base->game.texHeight);
-	// base->tex_f.png_addr = mlx_get_data_addr(base->tex_f.png_img, &base->tex_f.png_bits_per_pixel, &base->tex_f.png_line_length, &base->tex_f.png_endian);
-	// base->tex_c.png_img = mlx_png_file_to_image(base->mlx.mlx,"./Textures_png/stars.png", &base->game.texWidth, &base->game.texHeight);
-	// base->tex_c.png_addr = mlx_get_data_addr(base->tex_c.png_img, &base->tex_c.png_bits_per_pixel, &base->tex_c.png_line_length, &base->tex_c.png_endian);
-
-//i = 5: Ceiling, i = 6: Floor.
 
 static void		get_tex_coordinate(t_game *game, t_floor *floor)
 {
@@ -41,6 +31,10 @@ static void		get_tex_coordinate(t_game *game, t_floor *floor)
 	floor->floorx += floor->floorstepx;
 	floor->floory += floor->floorstepy;	
 }
+
+/*
+** Ceiling is symmetrical, at (screenHeight - y - 1) instead of y.
+*/
 
 static void		floor_ceiling_tex_x_loop(t_base *base, t_floor *floor,
 											t_tex *tex_fc, int y)
@@ -69,17 +63,19 @@ static void		floor_ceiling_tex_x_loop(t_base *base, t_floor *floor,
 }
 
 /*
-** -RAYdIR: for leftmost ray (x = 0) and rightmost ray (x = width screen)
-** -P: Current y position compared to the center of the screen (the horizon)
-** -POSz: Vertical position of the camera. With 0.5, it's exactly in the center
-** between floor and ceiling, matching also how the walls are being raycasted.
-** -ROWdISTANCE: Horizontal distance from the camera to the floor for the
-** current row. 0.5 is the z position exactly in the middle between floor
-** and ceiling.
-** -FLOORSTEP: calculate the real world step vector we have to add for each x
-** (parallel to camera plane). //CHANGED TO render_x to render_y (prettier).
-** -FLOOR: real world coordinates of the leftmost column.
-** This will be updated as we step to the right.
+** The camera ray goes through the following two points: 
+** 		1. The camera itself, which is at a certain height (posZ). 
+**		2. A point in front of the camera (through an imagined vertical plane)
+** Raydir: for leftmost ray (x = 0) and rightmost ray (x = width screen)
+** P: Current y position compared to the center of the screen (the horizon)
+** PosZ: Vertical position of the camera. With 0.5, it's exactly in the center
+** between floor and ceiling.
+** RowDistance: Horizontal distance from the camera to the floor for the
+** current row.
+** Floorstep: calculate the real world step vector we have to add for each x
+** (parallel to camera plane).
+** Floor: real world coordinates of the leftmost column. Will be updated as we
+** step to the right.
 */
 
 static void		floor_ceiling_tex_y(t_read *read, t_game *game, t_floor *floor
@@ -101,25 +97,9 @@ static void		floor_ceiling_tex_y(t_read *read, t_game *game, t_floor *floor
 		/ read->render_y;
 	floor->floorstepy = floor->rowdistance * (raydiry1 - raydiry0)
 		/ read->render_y;
-	floor->floorx = read->x_pos + floor->rowdistance * raydirx0; //changed double x_pos
+	floor->floorx = read->x_pos + floor->rowdistance * raydirx0;
 	floor->floory = read->y_pos + floor->rowdistance * raydiry0;
 }
-
-/*
-** Texturing the floor and ceiling is optional. 
-** The subject states a smooth color from the scene file.
-** to activate texturing:
-** activate the function in function "loop" in raycaster.c
-*/
-
-
-
-/*
-** mlx_png_file_to_image currently leaks memory. Therefore using xpm function
-** instead.
-** i = 0: North, i = 1: East, i = 2: South , i = 3: West, i = 4: Sprite
-** i = 5: Ceiling, i = 6: Floor.
-*/
 
 void				exit_bonus(t_base *base, t_tex *tex_fc)
 {
@@ -138,7 +118,12 @@ void				exit_bonus(t_base *base, t_tex *tex_fc)
 	exit_game(base, 1, 22); //
 }
 
-static void			load_floor_ceiling(t_base *base, t_tex *tex_fc, t_game *game, void *mlx)
+/*
+** i = 0: floor, i = 1: ceiling.
+*/
+
+static void			load_floor_ceiling(t_base *base, t_tex *tex_fc,
+										t_game *game, void *mlx)
 {
 	char	*path;
 	int		i;
@@ -154,11 +139,19 @@ static void			load_floor_ceiling(t_base *base, t_tex *tex_fc, t_game *game, void
 			&game->texheight);
 		if (tex_fc[i].xpm_img == NULL)
 			exit_bonus(base, tex_fc); //
-		tex_fc[i].xpm_addr = mlx_get_data_addr(tex_fc[i].xpm_img, &tex_fc[i].xpm_bpp,
-			&tex_fc[i].xpm_line_length, &tex_fc[i].xpm_endian);
+		tex_fc[i].xpm_addr = mlx_get_data_addr(tex_fc[i].xpm_img,
+			&tex_fc[i].xpm_bpp, &tex_fc[i].xpm_line_length,
+			&tex_fc[i].xpm_endian);
 		i++;
 	}
 }
+
+/*
+** Floor and ceiling textures are drawn horizontal. The perspective is similar
+** to that of walls but 90 degrees rotated, but unlike the walls which used
+** exactly 1 texture per vertical stripes, multiple floor textures may cross
+** our horizontal line.
+*/
 
 void			floor_ceiling_texture(t_base *base)
 {
